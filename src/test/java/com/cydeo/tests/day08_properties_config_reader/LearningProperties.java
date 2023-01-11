0ad6006cc1e2e9b0bb4d248eab6ebf243fca6967
Ali KARANLIK <alikaranlik3112@gmail.com>
2023-01-11 10:08:00 +0300

    day08_LearningProperties

--- src/test/java/com/cydeo/tests/day08_properties_config_reader/LearningProperties.java
+++ src/test/java/com/cydeo/tests/day08_properties_config_reader/LearningProperties.java
@@ -6,5 +6,6 @@ public class LearningProperties {
     @Test
     public void java_properties_reading_test(){
         System.out.println("System.getProperty(\"os.name\") = " + System.getProperty("os.name"));
+        System.out.println("System.getProperty(\"user.name\") = " + System.getProperty("user.name"));
     }
 }
