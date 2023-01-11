1eb23ce5d319db8c2c068e74063687fde979e48e
Ali KARANLIK <alikaranlik3112@gmail.com>
2023-01-11 12:16:46 +0300

    day08_T4_Config_Practice

---
+++ src/test/java/com/cydeo/tests/day08_properties_config_reader/T4_Config_Practice.java
@@ -0,0 +1,56 @@
+package com.cydeo.tests.day08_properties_config_reader;
+
+import com.cydeo.utulities.ConfigurationReader;
+import com.cydeo.utulities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class T4_Config_Practice {
+    public WebDriver driver;
+
+    @BeforeMethod
+    public void setupMethod() {
+        //TC #4: Google search
+        // 1- Open a chrome browser
+
+        // We are getting the browserType dynamically from our configuration.properties file
+        String browserType= ConfigurationReader.getProperty("browser");
+        driver=WebDriverFactory.getDriver(browserType);
+
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        // 2- Go to: https://google.com
+        driver.get("https://google.com");
+    }
+    @Test
+    public void google_search_test(){
+    // 3- Write “apple” in search box
+        WebElement searchbox= driver.findElement(By.xpath("//input[@name='q']"));
+        searchbox.sendKeys("apple"+ Keys.ENTER);
+    // 4- Verify title:
+    // Expected: apple - Google'da Ara
+        String expectedTitle="apple - Google'da Ara";
+        String actualTitle= driver.getTitle();
+        Assert.assertEquals(actualTitle,expectedTitle);
+
+    }
+
+//Use `configuration.properties` for the following:
+// 1. The browser type
+// 2. The URL
+// 3. The search keyword
+// 4. Make title verification dynamic.
+// If search value changes, title assertion should not fail.
+
+
+}
+
