3b0f54c4e39d2ed0a7355db53f80fc3115c3e9aa
Ali KARANLIK <alikaranlik3112@gmail.com>
2023-01-23 22:40:32 +0300

    ..

--- src/test/java/com/cydeo/tests/day06_alerts_iframes_windows/DropdownPractices.java
+++ src/test/java/com/cydeo/tests/day06_alerts_iframes_windows/DropdownPractices.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -83,8 +82,12 @@ public class DropdownPractices {
         Assert.assertEquals(actualMonth, expectedMonth);
         Assert.assertEquals(actualDay, expectedDay);
     }
+   /*
     @AfterMethod
     public void teardownMethod(){
         driver.close();
+
     }
+
+    */
 }
